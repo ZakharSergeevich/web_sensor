ed4eb71579cdf4f39851c7e93af37016205ad97b
Zakhar <zakhar-garbuzov@inbox.ru>
2017-07-13 13:19:36 +0300

    Создан проект Ардуино в VS2017

---
+++ sensor/web_sensor/web_sensor.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		web_sensor.ino
+ Created:	13.07.2017 13:16:09
+ Author:	zakha
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
